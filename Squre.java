e49f50a92d3947b7a9ae6a9bdb31052565ddd067
rgpv bhopal <139623664+Kush-Development@users.noreply.github.com>
2024-02-10 06:46:18 +0000

    commit files

---
+++ Squre.java
@@ -0,0 +1,22 @@
+/**
+ * Squre
+ */
+public class Squre {
+
+    public static void main(String[] args) {
+
+    // star pattren 
+
+for(int i = 0; i<=5; i++){
+    
+    for(int j =0; j<=5 ; j++){
+    System.out.print("* " );
+    }
+    System.out.println("");
+}
+
+
+    }
+}
+
+
