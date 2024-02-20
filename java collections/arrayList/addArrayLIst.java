4cf4db9a6adf6c46177cc6a40b85729d86bbff31
rgpv bhopal <139623664+Kush-Development@users.noreply.github.com>
2024-02-20 05:56:47 +0000

    commit

---
+++ java collections/arrayList/addArrayLIst.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+
+public class addArrayLIst{
+
+    public static void main(String[]args){
+
+System.out.println("hello");
+Arraylist<String> fruits= new Arraylist<>();
+fruits.add("Apple");
+fruits.add("Apple");
+fruits.add("Apple");
+fruits.add("Apple");
+fruits.add("Apple");
+
+
+System.out.println("Element in the ArrayList");
+Arraylist<String> k = fruits;
+for(String fruit : fruits){
+
+    System.out.println(fruit);
+
+}
+
+}
+}  S
